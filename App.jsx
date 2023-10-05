22e4e88a6485be0d8ad834e107752561501e0e9f
ABHIJIT-PADHY <126409698+abhijitpadhy@users.noreply.github.com>
2023-10-05 23:34:02 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+import Navigation from './components/Navigation';
+import Hero from './components/Hero'
+const App = () => {
+  return (
+    
+    <div>
+     <Navigation />
+     <Hero />
+    </div>
+  )
+}
+
+export default App
---
+++ Navigation.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav>
+    <div className='logo'>
+      <img src='/images/brand_logo.png' alt=''/>
+
+
+    </div>
+    <ul>
+      <li href="#">
+       ||  Menu ||
+      </li>
+      <li href="#">
+|| Location ||
+      </li>
+      <li href="#">
+      ||  About ||
+      </li>
+      <li href="#">
+       ||  Contact ||
+      </li>
+    </ul>
+    <button>Login</button>
+  </nav>
+  )
+}
+
+export default Navigation
